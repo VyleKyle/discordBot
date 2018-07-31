3bffb8332e2a52fc2e9a73406739ef3cc34a14c2
Kyle Noble <kyle.noble@comcast.net>
2018-07-31 01:35:38 -0700

    Added files
    discordBot is the main file to run. It pulls its commands from botCmds and uses pre to manage settings and shared variables.
    I know I could've made this bot a lot simpler if I used the ext.commands part of the discord API
    but where's the fun in that? I did this to challenge myself more than anything.
    
    You'll need to pip install discord.py which can be found here:
    https://github.com/Rapptz/discord.py

---
+++ pre.py
@@ -0,0 +1,41 @@
+import csv
+
+async def init(cli):
+    global settings
+    settings = {}
+    global admins
+    admins = {}
+    global banned
+    banned = {}
+    global toDoList
+    toDoList = {}
+    global client
+    client = cli
+    return
+
+async def writeSettings():
+    for server in client.servers:
+        with open("{}.csv".format(server.id), 'w', newline='') as file:
+            write = csv.writer(file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            write.writerow(settings[server.name])
+            write.writerow(admins[server.name])
+            write.writerow(banned[server.name])
+            write.writerow(toDoList[server.name])
+
+
+async def permCheck(lvl, m):
+    if m.author.id == "176473884919332864": #My uID
+        return True
+    elif (m.author.id in admins[m.server.name]) and (lvl == "admins"):
+        return True
+    elif (m.author.id in banned[m.server.name]):
+        await client.send_message(m.channel, "Error: User is banned.")
+        return False
+    elif lvl != "admins" and lvl != "kyle":
+        return True
+    else:
+        await client.send_message(m.channel, "Error! Insufficient permissions! Command permission level: {}".format(lvl))
+        return False
+
+
+
